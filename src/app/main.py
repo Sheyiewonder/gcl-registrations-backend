361952444529e712588e0152c5f388032e67c5cf
Sheyiewonder <sheyiewonder@gmail.com>
2026-08-11 14:59:11 -0700

    fix production CORS

--- src/app/main.py
+++ src/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://gcl-registrations-frontend-kappa.vercel.app"
+        "https://gcl-registrations-frontend-a584q6isf-sheyiewonders-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
